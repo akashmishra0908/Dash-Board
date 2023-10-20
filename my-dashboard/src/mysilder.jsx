222aa17015df720131fcee3650647bdfe4a69f60
akashmishra0908 <112822976+akashmishra0908@users.noreply.github.com>
2023-10-20 22:56:49 +0530

    completed

--- my-dashboard/src/mysilder.jsx
+++ my-dashboard/src/mysilder.jsx
@@ -6,87 +6,88 @@ import {
   Image,
   Flex,
   IconButton,
+  Button
 } from "@chakra-ui/react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
 const testimonials = [
   {
     id: 1,
-    name: "John Doe",
+    name: "DOT Batch",
     image:
       "https://worldywcacouncil.org/wp-content/uploads/2015/04/speaker-3-v2.jpg",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus eu erat vitae velit pretium vehicula vel ut libero.",
+      "by Love Babbar",
   },
   {
     id: 2,
-    name: "Jane Smith",
+    name: "DOT Batch",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQIi4cfvlez95aujoa9rdW6NaEO88l-9etvYIgHGoVoQhv6GDwEE8KahuKNhVKpuSUx9ZE&usqp=CAU",
     description:
-      "Phasellus euismod, dui vel tempus pellentesque, erat ex dapibus est, vel cursus neque ligula sit amet neque.",
+    "by Love Babbar",
   },
   {
     id: 3,
-    name: "Alice Johnson",
+    name: "DOT Batch",
     image:
       "https://think.ing.com/uploads/authors/_w480h480/James_Smith_new_profile.jpg",
     description:
-      "Cras ac quam eu neque interdum condimentum. Nunc congue justo nec augue dictum, ut eleifend mi vestibulum.",
+    "by Love Babbar",
   },
   {
     id: 4,
-    name: "Milly Boby",
+    name: "DOT Batch",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkmwhiR_rpYB1nFDpig8ccAiW9W5oNaZeJXQ&usqp=CAU",
     description:
-      "Nulla facilisi. Sed nec volutpat purus. Aenean vulputate tellus nec libero volutpat, in cursus tellus blandit.",
+    "by Love Babbar",
   },
   {
     id: 5,
-    name: "Ella Sara",
+    name: "DOT Batch",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIEnsHnmsZn2aeQyBLLib5FKD3XrYsBVDtmw&usqp=CAU",
     description:
-      "Fusce vestibulum, nulla ut cursus tristique, dolor velit posuere tortor, vel eleifend quam leo vel magna.",
+      "by Love Babbar",
   },
   {
     id: 6,
-    name: "Grace White",
+    name: "DOT Batch",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSRIIOJ4GCDG9j-lny0oSZtxMFhwgsHDBhABI75MBRNfRiqAgxMX3C9JZV4VDkytYs1fEI&usqp=CAU",
     description:
-      "Integer luctus fringilla nunc a egestas. Fusce tristique tellus a tristique vestibulum. Sed id lorem in dolor.",
+    "by Love Babbar",
   },
   {
     id: 7,
-    name: "David Clark",
+    name: "DOT Batch",
     image: "https://www.parliament.nz/media/1272/clark-david3.DmxK5Q.jpg",
     description:
-      "Praesent ac ex euismod, congue justo nec, sollicitudin nulla. Integer a congue augue. Vestibulum ante ipsum primis.",
+    "by Love Babbar",
   },
   {
     id: 8,
-    name: "Samuel Taylor",
+    name: "DOT Batch",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsjtkt7M-tv2LhI1z8i-N35EMHgX-M_gn2Sw&usqp=CAU",
     description:
-      "Vestibulum at dolor vel elit venenatis consectetur. Curabitur venenatis risus sed odio laoreet, quis auctor tortor.",
+    "by Love Babbar",
   },
   {
     id: 9,
-    name: "Olivia Davis",
+    name: "DOT Batch",
     image:
       "https://webapp4.asu.edu/photo-ws/directory_photo/ondavis1?size=medium&break=1695764228",
     description:
-      "Morbi commodo lectus ut erat fringilla, id venenatis est euismod. Vivamus a nulla id erat venenatis cursus.",
+    "by Love Babbar",
   },
   {
     id: 10,
-    name: "William Evans",
+    name: "DOT Batch",
     image:
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRPHzoQElSuUlSTUIAhy_3qJqZ7DGUpEqH8cQ&usqp=CAU",
     description:
-      "Sed bibendum nisl ut quam dictum, nec feugiat tortor placerat. Nam vel velit sed arcu ultricies tincidunt.",
+    "by Love Babbar",
   },
 ];
 const Slide = () => {
@@ -126,9 +127,6 @@ const Slide = () => {
   return (
     <Box bg="gray.100" py={12}>
       <Container maxW="container.lg" textAlign="center">
-        <Text fontSize="3xl" fontWeight="semibold" color="gray.800" mb={6}>
-          Featured Testimonials
-        </Text>
         <Flex alignItems="center" justifyContent="space-between">
           <IconButton
             bg="white"
@@ -155,6 +153,7 @@ const Slide = () => {
                     {testimonial.name}
                   </Text>
                   <Text color="gray.700">{testimonial.description}</Text>
+                  <Button w={"100%"} p={2} mt={5} bg="black" color={"white"}>View Course</Button>
                 </Box>
               </Box>
             ))}
